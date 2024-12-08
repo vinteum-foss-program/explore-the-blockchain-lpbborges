bb97525b691762df6d5def92ee85952cf89a8f2b
Luiz Paulo <luizpaulobborges@gmail.com>
2024-12-08 15:58:54 -0300

    fix: showing the address as output

--- submission/004.sh
+++ submission/004.sh
@@ -3,4 +3,4 @@
 
 descriptor=$(sed 's/^"//; s/"$//' <<< "$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)" | jq .descriptor)")
 
-bitcoin-cli deriveaddresses $descriptor
+bitcoin-cli deriveaddresses $descriptor | jq .[]
